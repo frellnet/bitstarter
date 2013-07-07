0b5940923d1abb10bd2bf76db4d4b76d104b99c8
Joe Goldberg <frellnet@gmail.com>
2013-07-07 20:31:39 +0000

    First Try with read Index

--- web.js
+++ web.js
@@ -6,6 +6,11 @@ app.get('/', function(request, response) {
   response.send('Hello World2!');
 });
 
+fs.readFileSync('index.html', function (data){
+  line = buf.toString();
+  response.send(line);
+}); 
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
